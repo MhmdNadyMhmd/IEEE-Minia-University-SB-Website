3b887b90d43574ed6bac218fa8b08e54e4fc3f36
MhmdNadyMhmd <mohamed.nady.m.ali@gmail.com>
2018-03-07 20:45:41 +0200

    add files

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* global $ window */
+$(function () {
+  'use strict';
+
+  // links add active class
+
+  // $('.navbar .navbar-nav .nav-item').click(function () {
+
+  //   $(this).addClass('active').siblings().removeClass('active');
+
+  // });
+
+
+  // smooth scroll to div
+
+  // $('.navbar .navbar-nav .nav-item a').click(function () {
+
+  //   $('html, body').animate({
+
+  //     scrollTop: $('#' + $(this).data('value')).offset().top
+
+  //   }, 1000);
+
+  // });
+
+});
